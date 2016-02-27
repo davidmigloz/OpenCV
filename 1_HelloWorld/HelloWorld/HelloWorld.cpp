8c0cdadb3c0a03b0cb7b92f58b6e9b4b39625c27
davidmigloz <me@davidmiguel.com>
2016-02-27 01:22:08 +0100

    Add HelloWorld project

---
+++ 1_HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <opencv2\opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	// Name of the image to load
+	char ImageName[] = "../img.jpg";
+	Mat image;
+
+	// Load image and check it is correct
+	image = cv::imread(ImageName);
+	if (!image.data) {
+		cout << "Error at loading image" << ImageName << endl;
+		exit(1);
+	}
+
+	// Show image
+	namedWindow("Original", CV_WINDOW_AUTOSIZE);
+	imshow("Original", image);
+
+	// Wait for a key to be pressed
+	waitKey(0);
+
+	return 0;
+}
